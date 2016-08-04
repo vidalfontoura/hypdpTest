7fc78c38e9a23913dd4d64827ce3ff172ff8a590
vdfontoura <vdfontoura@inf.ufpr.br>
2016-08-04 17:40:46 -0300

    COmmiting best results

---
+++ v1/run_main.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+for i in $(seq 1 30)
+do
+	#for j in $(seq 1 100)
+#	do
+	#echo $i $j
+		qsub run_hypdp_cluster.sh $i 9 6000000
+
+#	done
+done
---
+++ v2/sq8/run_hypdp_cluster.sh
@@ -0,0 +1,43 @@
+#/bin/bash
+#########################################
+## Parametros que le pasamos al script ##
+#########################################
+#$ -S /bin/bash
+#######################################
+# Usar el directorio de trabajo actual
+#######################################
+#$ -cwd
+# Tiempo de trabajo
+#$ -l h_rt=2400:00:00
+# juntar la salida estandar y de error en un solo fichero
+#$ -j y
+###########################
+# usar colas indicadas
+###########################
+#$ -q main.q
+# #$ -q 2014all.q
+##$ -t 1-30:1
+##$ -o /dev/null
+
+
+
+######################## Run the algorithm ################
+# echo ""
+echo "Host --> $HOSTNAME"
+
+echo Init Time: `date`
+init=`date +'%s'`
+
+echo "Seed: "+$1
+echo "Instance: "+$2
+echo "Time: "+$3
+
+java -Xmx3000M -Xms3000M -jar hypdpTest2.jar $1 $2 $3
+
+
+
+final=`date +'%s'`
+echo End  Time: `date` --- run in $(( (($final-$init)/3600) )):$(( (($final-$init)/60)%60 )):$(( (($final-$init))%60 )) ---
+
+######################## end of running the algorithm ################
+
